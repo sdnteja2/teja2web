0d348e1eb73518d087ecfd5aa8e152e7a54bf7ed
narr07 <dinarpermadi07@gmail.com>
2024-12-13 14:04:02 +0700

    Add Tailwind CSS and ESLint configuration, update Nuxt.js modules, and enhance .gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,13 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-12-13"
+  compatibilityDate: "2024-12-13",
+  modules: [
+    "@nuxthub/core",
+    "@nuxt/ui",
+    "@nuxt/content",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@vueuse/nuxt"
+  ]
 })
